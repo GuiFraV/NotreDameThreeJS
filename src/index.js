51820995209ce5a9cac6c990b4925ec8c047133c
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-09-25 21:47:45 +0200

    new setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <Canvas></Canvas>
+)
